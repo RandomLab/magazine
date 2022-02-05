7e3f03e392da5df7567e5fc10d34090e33d5b668
Jérémie Nuel <jeremie.nuel@pm.me>
2022-02-05 15:30:16 +0100

    continue

--- wiki2json/main.js
+++ wiki2json/main.js
@@ -1,27 +1,36 @@
 import { GraphQLClient, gql } from 'graphql-request'
 import 'dotenv/config'
+import * as fs from 'fs'
+
 
 async function main() {
-  const endpoint = 'https://wiki.reflux.media/graphql'
+    const endpoint = 'https://wiki.reflux.media/graphql'
 
-  const graphQLClient = new GraphQLClient(endpoint, {
-    headers: {
-      authorization: process.env.SECRET_KEY,
-    },
-  })
+    const graphQLClient = new GraphQLClient(endpoint, {
+        headers: {
+            authorization: process.env.SECRET_KEY,
+        },
+    })
 
-  const query = gql`
-    {
-        pages {
-            list(orderBy:ID) {
-                id
+    const query = gql`
+        {
+            pages {
+                list(orderBy:ID) {
+                    id
+                }
             }
         }
-    }
-  `
+    `
 
-  const data = await graphQLClient.request(query)
-  console.log(JSON.stringify(data, undefined, 2))
+    const data = await graphQLClient.request(query)
+    const data2file = JSON.stringify(data, undefined, 2)
+
+    fs.writeFile('data.json', data2file, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("JSON data is saved.");
+    })
 }
 
 main().catch((error) => console.error(error))
